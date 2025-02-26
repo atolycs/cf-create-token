332139be15ec95a31760320f0ebc6083ec6d5015
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-02-26 11:39:05 +0900

    test: test code

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,41 @@ export async function run(): Promise<void> {
       apiToken: additional_token,
     });
 
-    core.info("==> Checking Token Availables...");
+    core.info("==> Checking Generate only Token is Availables...");
 
     const response = await client.accounts.tokens.verify({
       account_id: account_id,
     });
 
     core.info(`Token Status: ${response.status}`);
+
+    core.info("==> Creating Pages Deploy Only Secure Token...");
+    let { result } = await client.accounts.tokens.permissionGroups.get({
+      account_id: account_id,
+    });
+    /*     const secure_pages_token = await client.accounts.tokens.create({
+      account_id: account_id,
+      name: "Github Action Tempolay Token",
+      policies: [
+        {
+          effect: "allow",
+          permission_groups: [
+            { name: permission_groups. }
+            {}
+          ],
+          resources: {
+            `com.cloudflare.api.account.zone.${account_id}`
+          }
+        }
+      ]
+    })
+  */
+    console.log(
+      result.map((i) => {
+        return i.name.includes("Pages");
+      }),
+    );
+    //console.log(permission_groups)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
